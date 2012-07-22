65f78a668f9f0c0dcaf2b537392590fc60f068df
Jonas <jonas@fuse.(none)>
2012-07-22 16:15:15 +0200

    Add result return for introspect, property decorator

--- cauliflower/model.py
+++ cauliflower/model.py
@@ -17,22 +17,29 @@ def boolean_field(**kwargs):
 class Field(object):
 
     def __init__(self, field_type, **kwargs):
-        self.field_type = field_type
+        self._field_type = field_type
         attributes = kwargs
+
+    @property
+    def field_type(self):
+        return self._field_type
 
 
 class Model(object):
 
     def __init__(self):
-        self.fields = {}
         forge = StorageForge()
+        # TODO: would be nicer to have class method
         self.storage = forge.build()
-        self._introspect()
+        self.fields = self._introspect()
+        self.storage.sync(self.fields)
 
     def _introspect(self):
         """Finds properties and registers them to use within adapter"""
+        fields = {}
         for attr_name in dir(self):
             attr = getattr(self, attr_name)
             if isinstance(attr, Field):
-                if attr_name not in self.fields:
-                    self.fields[attr_name] = attr
+                if attr_name not in fields:
+                    fields[attr_name] = attr
+        return fields
