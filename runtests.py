86afbc030a5539464484e274f645aad0ba6fc1e1
Jonas <jonas@fuse.(none)>
2012-07-22 12:17:08 +0200

    First discover when try access single tests

--- runtests.py
+++ runtests.py
@@ -8,7 +8,7 @@ tests within ``cauliflower.testsuite`` and run them.
 import sys
 import argparse
 
-# TODO: try to import unittest2 for python2.4-2.6 backward compatability
+# TODO: try to import unittest2 for python2.4-2.6 backward compatibility
 from unittest import TextTestRunner, defaultTestLoader
 
 
@@ -21,9 +21,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
     test = args.test
 
+    # needs to discover before accessing specific tests
+    test_suite = defaultTestLoader.discover(TESTSUITE_DIR)
+
     if test is not None:
         path = '.'.join((TESTSUITE_DIR, test))
         test_suite = defaultTestLoader.loadTestsFromName(path)
-    else:
-        test_suite = defaultTestLoader.discover(TESTSUITE_DIR)
+
     TextTestRunner(verbosity=1).run(test_suite)
